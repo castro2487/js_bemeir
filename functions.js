c07d17d5a05689d60040d3e62eb2e3afb4d3f652
Roger Castro <omarcastro18@gmail.com>
2019-10-29 19:08:43 +0100

    add next binary number array

--- functions.js
+++ functions.js
@@ -14,3 +14,18 @@ var reformat = function (str) {
     var result = str.replace(/[aeiou]/gi, '').toLowerCase();
     return result.charAt(0).toUpperCase() + result.slice(1);
 }
+
+var next_binary_number = function (elem) {
+    var size = elem.length
+    var lastArrayValue = elem[size - 1]
+
+    if (lastArrayValue === 0) {
+        elem.pop();
+        elem.push(1);
+    } else if (lastArrayValue === 1) {
+        elem.pop();
+        elem.push(0);
+        elem.push(0);
+    }
+    return elem
+}
