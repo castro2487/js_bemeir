90b84739fc1a05d0a803b29603c4b031d3c064a8
Roger Castro <omarcastro18@gmail.com>
2019-11-03 01:33:50 +0100

    solve issue when the input value should return a 2 elem length and was returning one

--- functions.js
+++ functions.js
@@ -1,21 +1,25 @@
 var repeat = function (elem) {
-    // returns an array with element elem repeated 3 times.
-    var n = 3;
-    var arr = [];
+  // returns an array with element elem repeated 3 times.
+  var n = 3;
+  var arr = [];
 
-    for (var i = 0; i <= n - 1; i++) {
-        arr = arr.concat(elem);
-    };
+  for (var i = 0; i <= n - 1; i++) {
+    arr = arr.concat(elem);
+  };
 
-    return arr;
+  return arr;
 };
 
 var reformat = function (str) {
-    var result = str.replace(/[aeiou]/gi, '').toLowerCase();
-    return result.charAt(0).toUpperCase() + result.slice(1);
+  var result = str.replace(/[aeiou]/gi, '').toLowerCase();
+  return result.charAt(0).toUpperCase() + result.slice(1);
 }
 
 var next_binary_number = function (elem) {
-    var transform = elem.join("")
-    return JSON.parse("["+((parseInt(transform,2)+1).toString(2).split(""))+"]")
+  var transform = elem.join("")
+  if (elem.length === 2 && elem[0] === 0 && elem[1] === 0) {
+    return [0, 1]
+  } else {
+    return JSON.parse("[" + ((parseInt(transform, 2) + 1).toString(2).split("")) + "]")
+  }
 }
